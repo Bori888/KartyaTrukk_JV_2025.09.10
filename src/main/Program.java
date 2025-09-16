7a6ddc6f00a45cb6cccd8b5cca5492b6614c5cec
Bori888 <ilyes.bori8@gmail.com>
2025-09-16 16:50:40 +0200

    T-X re jo

--- src/main/Program.java
+++ src/main/Program.java
@@ -21,7 +21,7 @@ public class Program {
     }
 
     private static void osszeallit() {
-        // 21 lap feltöltése (csak példaértékek)
+        // 21 lap feltöltése (4 színnel, az M színből max 3 lap)
         String[] szinek = {"P", "T", "Z", "M"};
         String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VII"};
 
@@ -39,20 +39,19 @@ public class Program {
         }
     }
 
-   private static void kirak() {
-    System.out.printf("%-12s%-12s%-12s%n", "1. oszlop", "2. oszlop", "3. oszlop");
+    private static void kirak() {
+        System.out.printf("%-12s%-12s%-12s%n", "1. oszlop", "2. oszlop", "3. oszlop");
 
-    for (int sor = 0; sor < 7; sor++) {
-        for (int oszlop = 0; oszlop < 3; oszlop++) {
-            int idx = sor * 3 + oszlop;
-            System.out.printf("%-12s", pakli[idx]);
+        for (int sor = 0; sor < 7; sor++) {
+            for (int oszlop = 0; oszlop < 3; oszlop++) {
+                int idx = sor * 3 + oszlop;
+                System.out.printf("%-12s", pakli[idx]);
+            }
+            System.out.println();
         }
-        System.out.println();
+
+        System.out.println("-----------------------------");
     }
-
-    System.out.println("-----------------------------");
-}
-
 
     private static int beker() {
         Scanner scanner = new Scanner(System.in);
@@ -67,52 +66,50 @@ public class Program {
         }
         return oszlop;
     }
-private static void kever(int oszlop) {
-    String[] oszlop1 = new String[7];
-    String[] oszlop2 = new String[7];
-    String[] oszlop3 = new String[7];
 
-    // Oszlopok kiszedése a pakliból (ahogy a kirakás is használja)
-    for (int i = 0; i < 7; i++) {
-        oszlop1[i] = pakli[i];
-        oszlop2[i] = pakli[i + 7];
-        oszlop3[i] = pakli[i + 14];
+    private static void kever(int oszlop) {
+        String[] oszlop1 = new String[7];
+        String[] oszlop2 = new String[7];
+        String[] oszlop3 = new String[7];
+
+        // Oszlopok kiszedése a pakliból a kirakás logikájával egyezően (soronként 3 oszlop)
+        for (int i = 0; i < 7; i++) {
+            oszlop1[i] = pakli[i * 3 + 0];
+            oszlop2[i] = pakli[i * 3 + 1];
+            oszlop3[i] = pakli[i * 3 + 2];
+        }
+
+        // Új pakli összeállítása: választott oszlop mindig középre kerül
+        String[] ujPakli = new String[21];
+
+        String[] bal = null, kozep = null, jobb = null;
+
+        switch (oszlop) {
+            case 1:
+                bal = oszlop2;
+                kozep = oszlop1;
+                jobb = oszlop3;
+                break;
+            case 2:
+                bal = oszlop1;
+                kozep = oszlop2;
+                jobb = oszlop3;
+                break;
+            case 3:
+                bal = oszlop1;
+                kozep = oszlop3;
+                jobb = oszlop2;
+                break;
+        }
+
+        for (int i = 0; i < 7; i++) {
+            ujPakli[i * 3 + 0] = bal[i];
+            ujPakli[i * 3 + 1] = kozep[i];
+            ujPakli[i * 3 + 2] = jobb[i];
+        }
+
+        pakli = ujPakli;
     }
-
-    // Az új paklit most is oszloponként fogjuk felépíteni!
-    String[] ujPakli = new String[21];
-
-    // Új sorrend: választott oszlop mindig középre kerül
-    String[] bal = null, kozep = null, jobb = null;
-
-    switch (oszlop) {
-        case 1:
-            bal = oszlop2;
-            kozep = oszlop1;
-            jobb = oszlop3;
-            break;
-        case 2:
-            bal = oszlop1;
-            kozep = oszlop2;
-            jobb = oszlop3;
-            break;
-        case 3:
-            bal = oszlop1;
-            kozep = oszlop3;
-            jobb = oszlop2;
-            break;
-    }
-
-    // Most a kirakáshoz illeszkedve: új pakli oszloponként (7-7-7)
-    for (int i = 0; i < 7; i++) {
-        ujPakli[i] = bal[i];
-        ujPakli[i + 7] = kozep[i];
-        ujPakli[i + 14] = jobb[i];
-    }
-
-    pakli = ujPakli;
-}
-
 
     private static void melyikVolt() {
         System.out.println("A gondolt lapod: " + pakli[10]);
